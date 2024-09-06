0710f62e63237914f458cb60cbacfd14f6afe676
KadirQuintero03 <kadireq1928@gmail.com>
2024-09-05 21:36:05 -0500

    corrigiendo algo bobo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { DashboardPageComponent } from './pages/private/dashboard-page/dashboard
 import { LoginPageComponent } from './pages/public/login-page/login-page.component';
 import { MoneyOptionsPageComponent } from './pages/private/money-options-page/money-options-page.component';
 import { ConfirmPageComponent } from './pages/private/confirm-page/confirm-page.component';
+import { OtherOptionPageComponent } from './pages/private/other-option-page/other-option-page.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { ConfirmPageComponent } from './pages/private/confirm-page/confirm-page.
     DashboardPageComponent,
     MoneyOptionsPageComponent,
     ConfirmPageComponent,
+    OtherOptionPageComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
